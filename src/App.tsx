cb1a1e07347abadc6d44fd40c9a937841fcebaf5
Mika Greif <mika.greif@googlemail.com>
2021-12-02 20:42:27 +0100

    added react-redux dependency

--- src/App.tsx
+++ src/App.tsx
@@ -5,43 +5,18 @@ import {
   Routes,
   Link
 } from 'react-router-dom'
-import { Layout, Menu, Breadcrumb } from 'antd';
 import 'antd/dist/antd.css';
-import { getMenuItemBasedOnUrl } from './libs/getMenuItemBasedOnUrl';
-import { ROUTE_CONFIGURATION } from './constants/appConfiguration';
-
-const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
-
+import { Provider } from 'react-redux'
+import Layout from './components/Layout'
+import { store } from './redux/store'
 
 function App() {
-
   return (
-    <BrowserRouter>
-      <Layout style={{ minHeight: '100vh' }}>
-        <Sider collapsible >
-          <Menu theme="dark" defaultSelectedKeys={[getMenuItemBasedOnUrl()]} mode="inline">
-            {
-              ROUTE_CONFIGURATION.map((config, index) => <Menu.Item key={(index + 1).toString()} icon={<config.icon />}>
-                <Link title={config.title} to={'/' + config.path}>{config.title}</Link>
-              </Menu.Item>
-              )}
-          </Menu>
-        </Sider>
-        <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }} />
-          <Content style={{ margin: '0 16px' }}>
-            <Routes>
-              {
-                ROUTE_CONFIGURATION.map(config => <Route path={'/' + config.path} element={<config.component />} />
-                )}
-              <Route path="*" element={<span>404 - Path not found</span>} />
-            </Routes>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>Tick It - Created By @Mika Greif</Footer>
-        </Layout>
-      </Layout>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
