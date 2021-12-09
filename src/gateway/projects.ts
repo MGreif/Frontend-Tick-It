ece188acc123c72db28c5081a52470c88d86ccdd
Mika Greif <mika.greif@googlemail.com>
2021-12-09 20:51:58 +0100

    Added gateway for API calls

---
+++ src/gateway/projects.ts
@@ -0,0 +1,17 @@
+import superagent from 'superagent'
+
+const baseUrl = `${process.env.REACT_APP_SERVICE_HOST}:${process.env.REACT_APP_SERVICE_PORT}`
+
+const fetchAllProjectsByUserId: any = (userId: string) => {
+  const url = baseUrl + '/projects/by-user/61ac08cd2a068d56f280c259'
+  return superagent
+    .get(url)
+}
+
+const fetchProjectData: any = (projectId: string) => {
+  const url = baseUrl + '/projects/' + projectId
+  return superagent
+    .get(url)
+}
+
+export { fetchAllProjectsByUserId, fetchProjectData }
