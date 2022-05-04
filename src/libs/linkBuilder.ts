ec508986e635dbfcd95593daa03877cccd72d91b
mgreif <mika.greif@googlemail.com>
2022-05-04 20:35:45 +0200

    Change API Link. Hardcoded API

--- src/libs/linkBuilder.ts
+++ src/libs/linkBuilder.ts
@@ -1,9 +1,8 @@
 export const buildApiLink = (url = '') =>
   `${process.env.REACT_APP_SERVICE_HOST}:${process.env.REACT_APP_SERVICE_PORT}${
     process.env.REACT_APP_SERVICE_PATH || ''
-  }${url}`
+  }/api${url}`
 
 export const buildRouterLink = (url = '') => {
-  console.log(`${process.env.REACT_APP_SERVICE_PATH || ''}${url}`)
   return `${process.env.REACT_APP_SERVICE_PATH || ''}${url}`
 }
