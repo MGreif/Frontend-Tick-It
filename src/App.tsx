4543e6b1ec2a24662f299e65e13ce9a6e1b0e2fb
mgreif <mika.greif@googlemail.com>
2022-05-09 18:07:56 +0200

    Add token check

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
-import {
-  BrowserRouter
-} from 'react-router-dom'
-import 'antd/dist/antd.css';
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import 'antd/dist/antd.css'
 import { Provider } from 'react-redux'
 import Layout from './components/Layout'
 import { store } from './redux/store'
+import { useCookies } from 'react-cookie'
 
-function App () {
+function App() {
+  const [cookies]: any = useCookies(['cookie-name'])
+
+  const token = cookies.token
+
+  console.log(token)
+
+  if (!token) {
+    document.location.href = '/auth/login'
+  }
+
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Layout />
       </BrowserRouter>
     </Provider>
-  );
+  )
 }
 
-export default App;
+export default App
