9ffcfa72a163c3b2c502d85e58dcdc4356763806
mgreif <mika.greif@googlemail.com>
2022-05-09 16:48:52 +0200

    Modify Linkbuilder

--- src/libs/linkBuilder.ts
+++ src/libs/linkBuilder.ts
@@ -1,8 +1,11 @@
-export const buildApiLink = (url = '') =>
-  `${process.env.REACT_APP_SERVICE_HOST}:${
-    process.env.REACT_APP_SERVICE_PORT
-  }/api${process.env.REACT_APP_SERVICE_PATH || ''}${url}`
+export const buildApiLink = (url = '') => {
+  const host = process.env.REACT_APP_SERVICE_HOST
+  const port = process.env.REACT_APP_SERVICE_PORT
+  const tickItServicePath = process.env.REACT_APP_SERVICE_PATH || ''
+  const apiRootPath = process.env.REACT_APP_API_ROOT_PATH || ''
+  return `${host}:${port}${apiRootPath}${tickItServicePath}${url}`
+}
 
 export const buildRouterLink = (url = '') => {
-  return `${process.env.REACT_APP_SERVICE_PATH || ''}${url}`
+  return `${process.env.REACT_APP_FRONTEND_ROOT_PATH || ''}${url}`
 }
